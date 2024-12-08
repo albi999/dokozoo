b8601dece1a2ef8dd976177db4953a258d625775
Albert Kniazev <al.kniazev2002@gmail.com>
2024-12-08 02:03:36 +0100

    reset kinda finished (not really). started to work on observe. cards2binary implemented

--- dokoenv/env/doko_cards.py
+++ dokoenv/env/doko_cards.py
@@ -1,6 +1,6 @@
 # Suits and ranks 
-SUITS = ['♦', '♥', '♠', '♣']
-RANKS = ['Q', 'J', 'A', '10', 'K']
+SUITS = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
+RANKS = ['Queen', 'Jack', 'Ace', '10', 'King']
 
 class Card:
     def __init__(self, suit, rank, isTrump=False):
@@ -15,17 +15,64 @@ class Card:
         self.isTrump = isTrump
 
     def __repr__(self):
-        return f"[{self.suit}{self.rank}{'*' if self.isTrump else ''}]"
+        # match case would have been prettier but this is python 3.9 
+        # match case needs python 3.10
+
+        s = None
+        r = None
+        if self.suit == 'Clubs':
+            s = '♣'
+        elif self.suit == 'Spades':
+            s = '♠'
+        elif self.suit == 'Hearts':
+            s = '♥'
+        elif self.suit == 'Diamonds':
+            s = '♦'
+
+        if self.rank == 'Queen':
+            r = 'Q'
+        elif self.rank == 'Jack':
+            r = 'J'
+        elif self.rank == 'Ace':
+            r = 'A'
+        elif self.rank == '10':
+            r = '10'
+        elif self.rank == 'King':
+            r = 'K'
+
+        return f"[{s}{r}{'*' if self.isTrump else ''}]"
+    
+    def __eq__(self, other):
+        # Ensure that the other object is also a Card
+        if not isinstance(other, Card):
+            return False
+        # Compare the suit, rank, and isTrump attributes
+        return (self.suit == other.suit and
+                self.rank == other.rank and
+                self.isTrump == other.isTrump)
 
 def create_deck():
     deck = []
-    for s in SUITS:
-        for r in RANKS:
-            if s=='♦' or r=='Q' or r=='J' or (s=='♥' and r=='10'):
-                deck.append(Card(s,r,True))
-                deck.append(Card(s,r,True))
-            else:
-                deck.append(Card(s,r,False))
-                deck.append(Card(s,r,False))
-    
+    # trump cards
+    deck.append(Card('Hearts', '10', True))
+    deck.append(Card('Hearts', '10', True))
+    for r in ['Queen', 'Jack']:
+        for s in SUITS:
+            deck.append(Card(s, r, True))
+            deck.append(Card(s, r, True))
+    for r in ['Ace', '10', 'King']:
+        deck.append(Card('Diamonds', r, True))
+        deck.append(Card('Diamonds', r, True))
+
+    # non-Trump cards
+    for r in ['Ace', '10', 'King']:
+        deck.append(Card('Clubs', r))
+        deck.append(Card('Clubs', r))
+    for r in ['Ace', '10', 'King']:
+        deck.append(Card('Spades', r))
+        deck.append(Card('Spades', r))
+    for r in ['Ace', 'King']:
+        deck.append(Card('Hearts', r))
+        deck.append(Card('Hearts', r))
+                 
     return deck
--- dokoenv/env/doko_environment.py
+++ dokoenv/env/doko_environment.py
@@ -1,16 +1,20 @@
-import functools
 import random
-from copy import copy
+from copy import copy, deepcopy
 import numpy as np
+from gymnasium.spaces import Discrete, MultiBinary, MultiDiscrete, Dict as SpaceDict
+# from gymnasium.utils import EzPickle
+# TODO: Pickleeeee
 
 
 from pettingzoo import AECEnv
+from pettingzoo.utils.agent_selector import agent_selector
 import doko_cards.py as cards
 
 class DokoEnvironment(AECEnv):
     metadata = {
         "name": "doko_environment_v0",
     }
+
 
     def __init__(self):
         """The init method takes in environment arguments.
@@ -28,12 +32,19 @@ class DokoEnvironment(AECEnv):
         These attributes should not be changed after initialization.
         """
         self.round = None
+        self.deck = None
         self.player1_cards = None
         self.player2_cards = None
         self.player3_cards = None
         self.player4_cards = None
-        self.possible_agents = ["player1", "player2", "player3", "player4"]
+        self.possible_agents = ['player1', 'player2', 'player3', 'player4']
+        self.starting_players = None
+        self.played_cards = None
+        # werde definitiv augen aufnehmen müssen
 
+    
+    
+    
     def reset(self, seed=None, options=None):
         """Reset set the environment to a starting point.
 
@@ -48,19 +59,76 @@ class DokoEnvironment(AECEnv):
         """
         self.agents = copy(self.possible_agents)
         self.round = 1
-        deck = cards.create_deck()
+
+        self.deck = cards.create_deck()
+        deck_copy = copy(self.deck)
+
+        random.shuffle(deck_copy)
+        self.player1_cards = self.cards2binary(deck_copy[:10])
+        self.player2_cards = self.cards2binary(deck_copy[10:20])
+        self.player3_cards = self.cards2binary(deck_copy[20:30])
+        self.player4_cards = self.cards2binary(deck_copy[30:40])
+
+        self.played_cards = np.zeros((10,4,40), dtype=bool)
+        self.starting_player = np.zeros(10, dtype=bool)
 
 
+        self._agent_selector = agent_selector(self.agents)
+        self.agent_selection = self._agent_selector.reset()
 
-    def step(self, actions):
-        pass
+        self.starting_players[0] = int(self.agent_selection[6])
+
+        """
+        ??????????????????????????????????????
+
+        self.rewards = {i: 0 for i in self.agents}
+        self._cumulative_rewards = {name: 0 for name in self.agents}
+        self.terminations = {i: False for i in self.agents}
+        self.truncations = {i: False for i in self.agents}
+        self.infos = {i: {} for i in self.agents}'
+        """
+
+    def step(self, action):
+        if (
+            self.terminations[self.agent_selection]
+            or self.truncations[self.agent_selection]
+        ):
+            return self._was_dead_step(action)
+        
+
+
+        
+
+    def observe(self, agent):
+        current_index = self.possible_agents.index(agent) # player1 has index 0 
+        observation = {
+            "round": self.round,
+            "starting_players": self.starting_players
+        }
+
+
+        return {"observation": observation, "action_mask": action_mask}
 
     def render(self):
         pass
 
-    def observation_space(self, agent):
-        return self.observation_spaces[agent]
+    def observation_space(self):
+        observation_space = SpaceDict({
+            "round": Discrete(10, start = 1),
+            "starting_players": ([4] * 10),
+            "cards_in_hand": MultiBinary(40),
+            "cards_played": MultiBinary([10,4,40]),
+            "action_mask": MultiBinary(10)
+        })
 
-    def action_space(self, agent):
-        return self.action_spaces[agent]
+        return observation_space
+
+    def action_space(self):
+        return Discrete(11-self.round)
     
+    # TODO:I dont need the whole env, just the deck which could be static
+    def cards2binary(self, cards): 
+        binary_representation = np.zeros(40, dtype=bool)
+        for card in cards:
+            binary_representation[self.deck.index(card)] = True
+        return binary_representation
