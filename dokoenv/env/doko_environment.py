77d76c037c0f01f83f69521fe3445590536b7f27
Albert Kniazev <al.kniazev2002@gmail.com>
2024-12-12 23:56:19 +0100

    doko_cards.py became obsolete, but it'll be needed later on, action_mask_calc and observe done

--- dokoenv/env/doko_environment.py
+++ dokoenv/env/doko_environment.py
@@ -8,8 +8,35 @@ from gymnasium.spaces import Discrete, MultiBinary, MultiDiscrete, Dict as Space
 
 from pettingzoo import AECEnv
 from pettingzoo.utils.agent_selector import agent_selector
-import doko_cards.py as cards
 
+
+
+
+
+"""
+i   action   card  
+--------------------
+1   0        [♥10*]
+2   1        [♣Q*] 
+3   2        [♠Q*] 
+4   3        [♥Q*] 
+5   4        [♦Q*] 
+6   5        [♣J*] 
+7   6        [♠J*] 
+8   7        [♥J*] 
+9   8        [♦J*] 
+10  9        [♦A*] 
+11  10       [♦10*]
+12  11       [♦K*] 
+13  12       [♣A]  
+14  13       [♣10] 
+15  14       [♣K]  
+16  15       [♠A]  
+17  16       [♠10] 
+18  17       [♠K]  
+19  18       [♥A]  
+20  19       [♥K]  
+"""
 class DokoEnvironment(AECEnv):
     metadata = {
         "name": "doko_environment_v0",
@@ -31,16 +58,15 @@ class DokoEnvironment(AECEnv):
 
         These attributes should not be changed after initialization.
         """
-        self.round = None
-        self.deck = None
-        self.player1_cards = None
-        self.player2_cards = None
-        self.player3_cards = None
-        self.player4_cards = None
+        # self.deck = None # I don't think I need it anymore. If I know 0 -> Hearts 10 and so forth, is more efficient I think
+
         self.possible_agents = ['player1', 'player2', 'player3', 'player4']
-        self.starting_players = None
-        self.played_cards = None
-        # werde definitiv augen aufnehmen müssen
+        self.round = None
+        self.player_cards = None
+        self.starter = None # player starting at the beginning of the game
+        self.cards_played = None
+        self.tricks_won_by = None
+        # TODO: werde definitiv augen aufnehmen müssen
 
     
     
@@ -57,36 +83,44 @@ class DokoEnvironment(AECEnv):
 
         And must set up the environment so that render(), step(), and observe() can be called without issues.
         """
+
         self.agents = copy(self.possible_agents)
-        self.round = 1
+        self.round = 0 # rounds 0 ... 9
 
-        self.deck = cards.create_deck()
-        deck_copy = copy(self.deck)
+        # dealing cards
+        deck = np.repeat(np.arange(1,21), 2)
+        random.Random(seed).shuffle(deck)
+        player1_cards = deck[:10]
+        player2_cards = deck[10:20]
+        player3_cards = deck[20:30]
+        player4_cards = deck[30:40]
+        self.player_cards = {
+            1: player1_cards,
+            2: player2_cards,
+            3: player3_cards,
+            4: player4_cards
+        }
 
-        random.shuffle(deck_copy)
-        self.player1_cards = self.cards2binary(deck_copy[:10])
-        self.player2_cards = self.cards2binary(deck_copy[10:20])
-        self.player3_cards = self.cards2binary(deck_copy[20:30])
-        self.player4_cards = self.cards2binary(deck_copy[30:40])
 
-        self.played_cards = np.zeros((10,4,40), dtype=bool)
-        self.starting_player = np.zeros(10, dtype=bool)
-
-
+        # TODO should be random who starts
         self._agent_selector = agent_selector(self.agents)
         self.agent_selection = self._agent_selector.reset()
+        self.starter = int(self.agent_selection[6]) # starter between 1 and 4 
 
-        self.starting_players[0] = int(self.agent_selection[6])
 
-        """
-        ??????????????????????????????????????
+        # no cards played so far, no tricks won by anyone
+        self.cards_played = np.zeros((10,4), dtype=np.int64)
+        self.tricks_won_by = np.zeros(10, dtype=np.int64)
 
+        
+
+        # copy paste shit
         self.rewards = {i: 0 for i in self.agents}
         self._cumulative_rewards = {name: 0 for name in self.agents}
         self.terminations = {i: False for i in self.agents}
         self.truncations = {i: False for i in self.agents}
-        self.infos = {i: {} for i in self.agents}'
-        """
+        self.infos = {i: {} for i in self.agents}
+        
 
     def step(self, action):
         if (
@@ -95,40 +129,109 @@ class DokoEnvironment(AECEnv):
         ):
             return self._was_dead_step(action)
         
+        current_agent = self.agent_selection
+
 
 
         
 
     def observe(self, agent):
-        current_index = self.possible_agents.index(agent) # player1 has index 0 
+        action_mask = self.action_mask_calc(self) if agent==self.agent_selection else []
+        player_number = int(self.agent_selection[6])
         observation = {
             "round": self.round,
-            "starting_players": self.starting_players
+            "cards_in_hand": self.player_cards[player_number], 
+            "cards_played": self.cards_played,
+            "tricks_won_by": self.tricks_won_by,
+            "action_mask": action_mask
         }
 
 
-        return {"observation": observation, "action_mask": action_mask}
+        return observation
 
     def render(self):
         pass
 
     def observation_space(self):
         observation_space = SpaceDict({
-            "round": Discrete(10, start = 1),
-            "starting_players": ([4] * 10),
-            "cards_in_hand": MultiBinary(40),
-            "cards_played": MultiBinary([10,4,40]),
-            "action_mask": MultiBinary(10)
+            "round": Discrete(10),
+            "cards_in_hand": MultiDiscrete(10 * [21]),
+            "cards_played": MultiDiscrete(10 * [4 * [21]]),
+            "tricks_won_by": MultiDiscrete(10 * [5]),
+            "action_mask": MultiBinary(20)
         })
-
         return observation_space
 
+    # 40 cards but because of Duplicates only 20 possible actions
+    # theoretically only max. 10 actions but this should work as well
     def action_space(self):
-        return Discrete(11-self.round)
+        return Discrete(20) 
     
-    # TODO:I dont need the whole env, just the deck which could be static
-    def cards2binary(self, cards): 
-        binary_representation = np.zeros(40, dtype=bool)
-        for card in cards:
-            binary_representation[self.deck.index(card)] = True
-        return binary_representation
+
+    def action_mask_calc(self):
+        # initializing the action mask
+        action_mask = np.zeros(20, dtype=np.int8)
+
+        # cards the selected agent could play
+        # [values between 0 and 20] 
+        # 0: nocard, 1: Hearts 10, 19: Hearts King 
+        cards = self.player_cards[int(self.agent_selection[6])] # player cards i --> player i
+        cards = cards[(1 <= cards)] # entry of 0 means there's no cards there
+        
+        # getting round and trick starter
+        r = self.round
+        trick_starter = self.starter if r == 0 else self.tricks_won_by[r-1] # P1-4
+        
+        # if agent starts a trick he can play any card he has
+        if trick_starter == int(self.agent_selection[6]): # player 1 to 4
+            return action_mask
+        
+        # determining which cards was played first
+        # value in [1,...,20]
+        # trickstarte - 1 cuz card_played indexing
+        firstcard = self.cards_played[r][trick_starter-1] 
+
+        # if first card Trump
+        if firstcard in range(1,13):
+            trumps = cards[(cards <= 12)]
+            if np.any(trumps):
+                action_mask[trumps-1] = 1 # trumps - 1 um korrespondierende action zu erhalten
+                return action_mask
+            else:
+                action_mask[cards-1] = 1
+                return action_mask
+        
+        # if first card non-Trump Clubs
+        if firstcard in range(13,16):
+            nont_clubs = cards[(13 <= cards) & (cards <= 15)]
+            if np.any(nont_clubs):
+                action_mask[nont_clubs-1] = 1
+                return action_mask
+            else: 
+                action_mask[cards-1] = 1
+                return action_mask
+        
+        # if first card non-Trump Spades
+        if firstcard in range(16,19):
+            nont_spades = cards[(16 <= cards) & (cards <= 18)]
+            if np.any(nont_spades):
+                action_mask[nont_spades-1]
+                return action_mask
+            else: 
+                action_mask[cards-1] = 1
+                return action_mask
+            
+        # if first card non-Trump Hearts
+        if firstcard in range(19,21):
+            nont_hearts = cards[(19 <= cards)]
+            if np.any(nont_hearts):
+                action_mask[nont_hearts-1] = 1
+                return action_mask 
+            else:
+                action_mask[cards-1] = 1
+                return action_mask
+
+        print("Something went wrong and your code fucking sucks")  
+        return action_mask
+
+
